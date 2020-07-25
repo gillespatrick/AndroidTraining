f66fb0b160dc4a987df5a52b36de30a0c31efba3
gilles <ngondogillespatrick@gmail.com>
2020-07-24 23:52:24 -0400

    Create and use variable

--- app/src/main/java/com/kotlin/Main.kt
+++ app/src/main/java/com/kotlin/Main.kt
@@ -1,8 +1,21 @@
 package com.kotlin
 
 fun main(){
+
+    val age = 25 * 365
+    val name = "Gilles"
     println()
-    println("Happy Birthday Gilles ")
-    println("Welcome to my party")
-    println("You're 25 yeas old")
+    println("Happy Birthday ${name} ")
+    println()
+
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println()
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
 }
