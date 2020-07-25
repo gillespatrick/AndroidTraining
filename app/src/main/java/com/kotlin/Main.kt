7b71a785c1e9d6793fce31938a6b6df0854639a2
gilles <ngondogillespatrick@gmail.com>
2020-07-25 00:06:58 -0400

    Create a birthday banner with a border

--- app/src/main/java/com/kotlin/Main.kt
+++ app/src/main/java/com/kotlin/Main.kt
@@ -3,9 +3,13 @@ package com.kotlin
 fun main(){
 
     val age = 25 * 365
+    val y = 4
     val name = "Gilles"
+    val x = "-._,-"
     println()
+    printBorder(x,y)
     println("Happy Birthday ${name} ")
+    printBorder(x,y)
     println()
 
     // Let's print a cake!
@@ -18,4 +22,12 @@ fun main(){
     println()
     println("You are already ${age} days old, ${name}!")
     println("${age} days old is the very best age to celebrate!")
+}
+
+fun printBorder(border:String, timeToRepeat:Int){
+    repeat(timeToRepeat){
+        print(border)
+    }
+    println()
+
 }
